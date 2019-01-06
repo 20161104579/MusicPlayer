522a5cb93f100487ad512839b0a1f10a601bc26e
Andy Chen <chenrui@chenruitekimacbook-air.local>
2019-01-06 12:21:35 +0800

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by chenrui on 2019/1/6.
+//  Copyright © 2019年 Andy Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
